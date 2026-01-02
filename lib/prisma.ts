81ebba558e855e60b5ffa9218479b7ec28f491a5
rishSaini <saini.rishabh895@gmail.com>
2026-01-02 15:04:07 -0700

    Added Neon adapter.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter: new PrismaNeon({ connectionString: process.env.DATABASE_URL! }),
     log: ["error", "warn"],
   });
 
